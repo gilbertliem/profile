6f1db9d9d9be2068f894b4e080e74621f4d5c3a3
gilbert yohanes <yohanesbert97@gmail.com>
2020-12-11 13:42:20 +0700

    adding website screenshot

--- src/components/Website/Website.jsx
+++ src/components/Website/Website.jsx
@@ -4,14 +4,16 @@ import styles from "./Website.module.css";
 export default function Website() {
   return (
     <>
-      <div className={styles.one}>
-        <div>FERCHAEL</div>
-      </div>
-      <div className={styles.two}>
-        <div>TALIKASIH</div>
-      </div>
-      <div className={styles.three}>
-        <div>MINIPROJECT</div>
+      <div className={styles.container}>
+        <div className={styles.one}>
+          <div className={styles.ferchael}>FERCHAEL</div>
+        </div>
+        <div className={styles.two}>
+          <div className={styles.talikasih}>TALIKASIH</div>
+        </div>
+        <div className={styles.three}>
+          <div className={styles.miniproject}>MINIPROJECT</div>
+        </div>
       </div>
     </>
   );
