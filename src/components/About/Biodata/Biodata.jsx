894172d4fa5cf24e9dd5201880bb3d123f143af1
gilbert yohanes <yohanesbert97@gmail.com>
2020-12-21 22:18:40 +0700

    styling biodata

--- src/components/About/Biodata/Biodata.jsx
+++ src/components/About/Biodata/Biodata.jsx
@@ -7,65 +7,81 @@ export default function Biodata() {
       <aside>
         <div className={styles.container}>
           <div className={styles.subhead}>contact</div>
-          <div className={styles.contact}>phone: </div>
-          <div className={styles.detail}>+ 62 817 378 555</div>
-          <div className={styles.contact}>email: </div>
-          <div className={styles.detail}>yohanesbert97@gmail.com</div>
-          <div className={styles.contact}>skype: </div>
-          <div className={styles.detail}>gilbertliem</div>
-          <div className={styles.contact}>linkedin: </div>
-          <div className={styles.detail}>
-            https://www.linkedin.com/in/gilbert-yohanes-052090119/
+          <div className={styles.col}>
+            <div className={styles.contact}>phone: </div>
+            <div className={styles.detail}>+ 62 817 378 555</div>
           </div>
-          <div className={styles.contact}>github: </div>
-          <div className={styles.detail}>https://github.com/gilbertliem</div>
+          <div className={styles.col}>
+            <div className={styles.contact}>email: </div>
+            <div className={styles.detail}>yohanesbert97@gmail.com</div>
+          </div>
+          <div className={styles.col}>
+            <div className={styles.contact}>skype: </div>
+            <div className={styles.detail}>gilbertliem</div>
+          </div>
+          <div className={styles.col}>
+            <div className={styles.contact}>linkedin: </div>
+            <div className={styles.detail}>
+              https://www.linkedin.com/in/gilbert-yohanes-052090119/
+            </div>
+          </div>
+          <div className={styles.col}>
+            <div className={styles.contact}>github: </div>
+            <div className={styles.detail}>https://github.com/gilbertliem</div>
+          </div>
         </div>
         <div className={styles.line}></div>
         <div className={styles.container}>
           <div className={styles.subhead}>education</div>
-          <div className={styles.name}>Glints Academy</div>
-          <div className={styles.education}>
-            Software Engineer
-            <br />
-            August 2020 - November 2020
+          <div className={styles.col}>
+            <div className={styles.name}>Glints Academy</div>
+            <div className={styles.education}>
+              Software Engineer
+              <br />
+              August 2020 - November 2020
+            </div>
           </div>
-          <div className={styles.name}>Universitas Ciputra Surabaya</div>
-          <div className={styles.education}>
-            Bachelor's Degree in Hotel and Tourism Business
-            <br />
-            July 2015 - July 2019
-            <br />
-            GPA 3.55 / 4.0
+          <div className={styles.col}>
+            <div className={styles.name}>Universitas Ciputra Surabaya</div>
+            <div className={styles.education}>
+              Bachelor's Degree in Hotel and Tourism Business
+              <br />
+              July 2015 - July 2019
+              <br />
+              GPA 3.55 / 4.0
+            </div>
           </div>
         </div>
         <div className={styles.line}></div>
         <div className={styles.container}>
           <div className={styles.subhead}>expertise</div>
-          <ul>
-            <li className={styles.list}>Javascript</li>
-            <li className={styles.list}>HTML</li>
-            <li className={styles.list}>CSS</li>
-            <li className={styles.list}>GIT, Heroku</li>
-            <li className={styles.list}>React JS</li>
-            <li className={styles.list}>Redux</li>
-            <li className={styles.list}>NPM</li>
-            <li className={styles.list}>API</li>
-            <li className={styles.list}>Microsoft Office</li>
-            <li className={styles.list}>Adobe</li>
-            <li className={styles.list}>Sales & Marketing</li>
+          <ul className={styles.list}>
+            <li>Javascript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>GIT, Heroku</li>
+            <li>React JS</li>
+            <li>Redux</li>
+            <li>NPM</li>
+            <li>API</li>
+            <li>Microsoft Office</li>
+            <li>Adobe</li>
+            <li>Sales & Marketing</li>
           </ul>
         </div>
         <div className={styles.line}></div>
         <div className={styles.container}>
           <div className={styles.subhead}>language</div>
           <div className={styles.language}>
-            <strong>english</strong> - intermediate
-          </div>
-          <div className={styles.language}>
-            <strong>bahasa indonesia</strong> - native
-          </div>
-          <div className={styles.language}>
-            <strong>mandarin</strong> - beginner
+            <div>
+              <strong>english</strong> - intermediate
+            </div>
+            <div>
+              <strong>bahasa indonesia</strong> - native
+            </div>
+            <div>
+              <strong>mandarin</strong> - beginner
+            </div>
           </div>
         </div>
       </aside>
