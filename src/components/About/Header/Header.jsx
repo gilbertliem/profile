b2a62ef596e1ba37d16473d49dc32932e9c91800
gilbert yohanes <yohanesbert97@gmail.com>
2020-12-21 23:26:22 +0700

    styling header

--- src/components/About/Header/Header.jsx
+++ src/components/About/Header/Header.jsx
@@ -10,8 +10,16 @@ export default function Header() {
         <div className={styles.col}>
           <div className={styles.name}>gilbert yohanes</div>
           <div className={styles.summary}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-            fugiat ipsum! Sit, inventore ab rem sunt quo sed numquam totam.
+            Gilbert is a graduate software engineer student. He took Frontend
+            Developer as the main subject, and focus on designing website,
+            creating friendly user interface, and working with APIs. Before he
+            takes software engineer academy program, he worked as a sales
+            executive in a company in Jakarta. He decided to change his career
+            path because he found himself enjoy and curious at the same time
+            about programming (coding), and he wants to focus on software
+            engineer as his career. He has made some projects for your
+            references (information in Portfolio area), kindly explore this
+            website and check his works out!
           </div>
         </div>
       </div>
